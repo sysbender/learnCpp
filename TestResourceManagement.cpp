5a1ba2bba1af2cc132f9766936d37c1a48c172fe
Jason <sysbender@gmail.com>
2022-12-26 14:49:39 -0500

    classic resource management - smart pointer

--- TestResourceManagement.cpp
+++ TestResourceManagement.cpp
@@ -15,42 +15,47 @@ namespace ResourceManagement {
 	public:
 
 
-		class Person {
-			string* pname_;
-		public:
-			Person(string name) { pname_ = new string(name); tlog << "\n create person: " << name; }
-			~Person() { 
-				if (pname_) {
-					tlog << "\n deleting Person: " << *pname_;
-					delete pname_;
-				}
-				else {
-					tlog << "\n deleting Person = invalid pointer ";
-				}
-			}
-			void printPerson() { tlog << "\n print Person : " << *pname_; }
-			// solution 1: define copy constructor and copy assign operator for deep copying
-			// solution 2: delete copy constructor and copy assign operator
 
-			// use explicit clone , instead of implicit copying
-			Person* clone() {
-				return (new Person( *pname_));  
-			}
-		private:
-
-			// avoid implicit copying
-			Person(const Person& rhs) {
-				pname_ = new string(*rhs.pname_);
-			}
-
-			Person operator=(const Person& rhs) {
-				pname_ = new string(*rhs.pname_);
-			}
-
-
-		};
 		TEST_METHOD(test_rm)
 		{
+
+			class Person {
+				string* pname_;
+			public:
+				Person(string name) { pname_ = new string(name); tlog << "\n create person: " << name; }
+				~Person() {
+					if (pname_) {
+						tlog << "\n deleting Person: " << *pname_;
+						delete pname_;
+					}
+					else {
+						tlog << "\n deleting Person = invalid pointer ";
+					}
+				}
+				void printPerson() { tlog << "\n print Person : " << *pname_; }
+				// solution 1: define copy constructor and copy assign operator for deep copying
+				// solution 2: delete copy constructor and copy assign operator
+
+				// use explicit clone , instead of implicit copying
+				Person* clone() {
+					return (new Person(*pname_));
+				}
+			private:
+
+				// avoid implicit copying
+				Person(const Person& rhs) {
+					pname_ = new string(*rhs.pname_);
+				}
+
+				Person operator=(const Person& rhs) {
+					pname_ = new string(*rhs.pname_);
+				}
+
+
+			};
+
+
+
 			Person p("tom");  //
 			p.printPerson();
 			//Person p2(Person("t"));
@@ -69,6 +74,54 @@ namespace ResourceManagement {
 
 
 		}
+
+		/*
+		* 1. cpp11 - =delete; cpp03 - make it private
+		* 2. cpp11 - vector.emplace_back() - constructor in place without copying in container
+		* 3. use shared_ptr to count , costly
+		* 4. use unique_ptr, and move semantics
+		
+		*/
+
+		TEST_METHOD(test_resource_management_11) {
+
+			class Person {
+				unique_ptr<string> pname_;
+			public:
+				Person(string name) { pname_ = make_unique<string>(name); tlog << "\n create person: " << name; }
+				//~Person() {
+				//	if (pname_) {
+				//		tlog << "\n deleting Person: " << *pname_;
+				//		delete pname_;
+				//	}
+				//	else {
+				//		tlog << "\n deleting Person = invalid pointer ";
+				//	}
+				//}
+				void printPerson() { tlog << "\n print Person : " << *pname_; }
+
+				~Person() { tlog << "\n destroy Person. "; };
+				Person(Person&&) = default; // if destructor is provided, explicitly add the move constructor
+
+			};
+
+
+			// emplace
+
+			vector<Person> persons;
+			persons.push_back( Person("Bill"));
+			Person p2("David");
+			persons.push_back(move(p2));
+			persons.back().printPerson();
+
+
+
+
+		}
+
+
+
+
 
 
 	};
